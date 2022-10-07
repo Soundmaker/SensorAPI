c615daf6457b810f14e7725230c10e71f5dbe010
Soundmaker <soundmakersoz@gmail.com>
2022-10-07 14:04:18 +0300

    Init app

---
+++ src/test/java/com/qwerty/SensorAPI/SensorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qwerty.SensorAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
