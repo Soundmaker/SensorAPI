4f457e8773dc4f0714b434e27322ed2b9c15f64d
Soundmaker <soundmakersoz@gmail.com>
2022-10-07 16:23:22 +0300

    add models

---
+++ src/main/java/com/qwerty/SensorAPI/models/Sensor.java
@@ -0,0 +1,36 @@
+package com.qwerty.SensorAPI.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "sensor")
+public class Sensor implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    @NotEmpty(message = "Название не должно быть пустым")
+    @Size(min = 3,max = 30, message = "Название сенсора должно быть от 3 до 30 символов")
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
