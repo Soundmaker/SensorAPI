cd419f3d196604b406b807c2d70c163d8bb505d3
Soundmaker <soundmakersoz@gmail.com>
2022-10-08 14:56:21 +0300

    Delete unused dependency

--- src/main/java/com/qwerty/SensorAPI/repositories/MeasurementRepository.java
+++ src/main/java/com/qwerty/SensorAPI/repositories/MeasurementRepository.java
@@ -3,7 +3,5 @@ package com.qwerty.SensorAPI.repositories;
 import com.qwerty.SensorAPI.models.Measurement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface MeasurementRepository extends JpaRepository<Measurement,Integer> {
 }
